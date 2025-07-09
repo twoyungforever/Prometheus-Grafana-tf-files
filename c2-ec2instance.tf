5307721d897087d55dac9e242a92a1f1bae790a8
Twoyungforever <nkosimoyoranen@gmail.com>
2025-07-09 14:02:31 +0200

    Change made to c2-ec2-file

--- c2-ec2instance.tf
+++ c2-ec2instance.tf
@@ -1,17 +1,3 @@
-/*
-
-# Resource: EC2 Instance
-resource "aws_instance" "myec2vm" {
-  ami = "ami-0742b4e673072066f"
-  instance_type = "t3.micro"
-  user_data = file("${path.module}/app1-install.sh")
-  tags = {
-    "Name" = "EC2 Demo"
-  }
-}
-
-*/
-
 resource "aws_instance" "myec3" {
   ami = "ami-0866a3c8686eaeeba"
   instance_type = var.ec2_instance_type 
@@ -22,7 +8,7 @@ resource "aws_instance" "myec3" {
     volume_type = "gp2"
   }
   tags = {
-    "Name" = "Sonar" 
+    "Name" = "Prometheus-Grafana-Server" 
   }
 }
 
