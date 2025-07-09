bab4ad21ea4abdb8dbcdc1a0c617ebcef90c5b5e
Twoyungforever <nkosimoyoranen@gmail.com>
2025-07-09 14:14:34 +0200

    Changes made to comments on versions.tf

--- c1-versions.tf
+++ c1-versions.tf
@@ -1,27 +1,5 @@
 # Terraform Block
 
-/*
-
-terraform {
-  required_version = ">= 1.6" # which means any version equal & above 0.14 like 0.15, 0.16 etc and < 1.xx
-  required_providers {
-    aws = {
-      source = "hashicorp/aws"
-      version = ">= 5.0"
-    }
-  } 
-
-}  
-# Provider Block
-provider "aws" {
-  region = "us-east-1"
-}
-
-/*
-Note-1:  AWS Credentials Profile (profile = "default") configured on your local desktop terminal  
-$HOME/.aws/credentials
-*/
-
 terraform {
   required_version = "~> 1.7.3"  #allows right most version to be used
   required_providers {
@@ -31,6 +9,8 @@ terraform {
     }
   }
 }
+
+# Providers Block
 
 provider "aws" {
   region = var.aws_region
